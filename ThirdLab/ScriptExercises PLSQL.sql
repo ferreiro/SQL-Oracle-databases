992c91a0237fe93c836e4f320d198ba1f01debfa
George <me@jgferreiro.com>
2015-12-15 11:00:53 +0100

    Update ScriptExercises PLSQL.sql

--- ThirdLab/ScriptExercises PLSQL.sql
+++ ThirdLab/ScriptExercises PLSQL.sql
@@ -99,11 +99,95 @@ BEGIN
   WHEN NO_DATA_FOUND THEN
     if (v_first=true) then
       DBMS_OUTPUT.PUT_LINE('Excepcion en la primera consulta... No hemos encontrado cursos con ese código');
+      return false;
     end if;
     if (v_second=true) then
       DBMS_OUTPUT.PUT_LINE('Excepcion en la SEGUNDA consulta... No hemos encontrado estudiantes enrolled en ese curso');
+      DBMS_OUTPUT.PUT_LINE('Por lo tanto. Todo esta bien :-)');
+      return true;
     end if;
-  WHEN TOO_MANY_ROWS THEN
+  /*WHEN TOO_MANY_ROWS THEN
     DBMS_OUTPUT.PUT_LINE ('Several organizations found'); 
+  */
   
 END;
+
+
+/*
+  b.	Write a stored procedure to enroll a student in a given course. 
+  First, you must check that the quota has not been exceeded (invoke the function previously designed); 
+  If the student cannot be enrolled, show a message that indicates it. 
+  The procedure will receive 3 parameters: course code, student id and a boolean that indicates if 
+  the student was enrolled previously in other course. In the case of regular and intensive courses 
+  the total price of the course will be the one stored in the table Fees. The price associated with 
+  the private courses is the price of one hour. In order to calculate the final price, you have to 
+  multiply it by the number of hours of each course.  All the students that were enrolled previously 
+  in a course will obtain a 5% discount.
+*/
+
+//
+CREATE OR REPLACE PROCEDURE EnrollStudent
+(p_code CHAR, p_IDStudent VARCHAR, p_oldStudent BOOLEAN)
+IS
+  
+  v_enrolled number:=0;
+  v_duplicated boolean:=true;
+  v_availableInscriptions boolean:=false;
+  
+  v_total number(3); -- total price for a course
+  v_type varchar(20);
+  v_price number(3);
+  v_hours number(3);
+
+BEGIN
+  
+  -- Check if student has been alreadfy in the courase / Duplicate key (student enrolled previosly?): print and returns
+  -- NVL= si nulo pones cero en v_enrolled
+  
+  SELECT NVL(COUNT(ID_STUDENT),0) into v_enrolled
+  FROM INSCRIPTIONS 
+  WHERE Inscriptions.Code = p_code and Inscriptions.ID_Student=p_IDStudent;
+  
+  if (v_enrolled=1) then
+    DBMS_OUTPUT.PUT_LINE('This student has been already enrolled');    
+  else
+     if(isGreaterQuote(p_code)) then
+        Select TypeF, price, hours into v_type, v_price,v_hours
+        FROM Courses natural join fees
+        WHERE Courses.code=p_code;
+        
+        -- Calculate price
+         
+        if (v_type='Private') then
+          v_total:=v_price*v_hours; -- Private courses are hours by the price.
+        else
+          v_total:=v_price;
+        end if;
+        
+        if (p_oldStudent=true) then
+          v_total:=v_total*0.95) -- Apply disccount: 5% if p_oldStudent
+        end if;
+        
+        
+        -- TODO
+        -- INSERT INTO TABLE
+        
+        
+      else
+        -- Show error: quota is exceded..
+        -- Quota limite for this course?: print and return
+     end if;
+  end if;
+   
+  EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    if (v_duplicated=true) then
+      DBMS_OUTPUT.PUT_LINE('This student has been already enrolled');
+      return false;
+    end if;
+    
+END;
+
+
+
+
